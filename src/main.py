5a7c5acf8578ee1967968c0c1cdefd78716d2d48
HighPon <s.shiraki.business@gmail.com>
2024-01-13 14:27:42 +0900

    merge

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ class App:
         pyxel.init(self.WIDTH, self.HEIGHT, title="Pyxel Display PNG")
         pyxel.images[0].load(0, 0, "assets/pipo-xmaschara03.png")
         pyxel.images[1].load(0, 0, "assets/map.png")
+        self.player = Player(0, 0, 0, 64, 64, 32, 32, 11)
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -16,7 +17,32 @@ class App:
     def draw(self):
         pyxel.cls(0)
         self.draw_field()
-        pyxel.blt(0, 0, 0, 64, 64, 32, 32, 11)
+        pyxel.blt(
+            self.player.x,
+            self.player.y,
+            self.player.img,
+            self.player.u,
+            self.player.v,
+            self.player.w,
+            self.player.h,
+            self.player.colkey)
+
+class Player:
+    def __init__(self, x, y, img, u, v, w, h, colkey):
+        self.x = x
+        self.y = y
+        self.img = img
+        self.u = u
+        self.v = v
+        self.w = w
+        self.h = h
+        self.colkey = colkey
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, self.img, self.u, self.v, self.w, self.h, self.colkey)
 
     def draw_field(self):
         pyxel.rect(0, 0, self.WIDTH, self.HEIGHT-16, 12)
