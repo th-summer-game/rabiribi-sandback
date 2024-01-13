c34a13a37cedf2ad5c7311a0765abcb16d09fd3b
HighPon <s.shiraki.business@gmail.com>
2024-01-13 13:22:22 +0900

    fix

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import pyxel
 class App:
     def __init__(self):
         pyxel.init(160, 120, title="Pyxel Display PNG")
-        pyxel.images[0].load(0, 0, "assets/pipo-xmaschara03.png")
+        pyxel.images[0].load(0, 0, "assets/pipo-xmaschara03.jpg")
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -11,6 +11,6 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.blt(0, 0, 0, 64, 64, 32, 32)
+        pyxel.blt(0, 0, 0, 64, 64, 32, 32, 13)
 
 App()
