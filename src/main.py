c12df958b573c5c862ca4fb6b3f09c2b948d0b9a
tuziemon <tuziemon@hotmail.co.jp>
2024-01-13 14:23:24 +0900

    sky and clouds forever

--- src/main.py
+++ src/main.py
@@ -19,7 +19,10 @@ class App:
         pyxel.blt(0, 0, 0, 64, 64, 32, 32, 11)
 
     def draw_field(self):
+        pyxel.rect(0, 0, self.WIDTH, self.HEIGHT-16, 12)
         for x in range(0, self.WIDTH, 16):
             pyxel.blt(x, self.HEIGHT-16, 1, 0, 0, 16, 16)
-    
+        for i in range(int((self.WIDTH)/32)+2):
+            pyxel.blt(i*32 - int(pyxel.frame_count/10)%64, int(self.WIDTH/8)*(i%2)+int(self.WIDTH/8), 1, 16, 0, 16, 16, 11)
+
 App()
